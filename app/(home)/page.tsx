2283205d28894bb6f514881d424ed29cafbe6cf0
vbnm134678 <syj1351@naver.com>
2024-06-24 19:19:18 +0900

    home page 애니메이션 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,15 @@
+import Hero from "@/components/hero"
 export const metadata = {
   title: 'HomePage',
 }
 
 export default function Home() {
   return (
-    <>
-      <main className="underline">
-        Main
-      </main>
-    </>
+    <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <Hero />
+      </div>
+    </section>
+
   )
 }
