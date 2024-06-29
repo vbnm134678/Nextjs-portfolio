6b0155be4f8f9a31e4113ad8f0da5906b5f0e442
vbnm134678 <syj1351@naver.com>
2024-06-29 17:53:30 +0900

    useTheme(darkmode) 관련 에러 메시지 수정

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 export const Providers: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isMount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true)
+  }, [])
+
+  if (!isMount) {
+    return null;
+  }
   return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 };
 
