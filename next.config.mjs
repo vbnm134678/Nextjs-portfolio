227bd5f0d8484b7f95a4536dd07c0606d25718d3
vbnm134678 <syj1351@naver.com>
2024-07-17 16:03:53 +0900

    next.js Image를 위한 config 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "/06eea82f-0a3c-4a47-b087-2eedf7c2ae0c/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
