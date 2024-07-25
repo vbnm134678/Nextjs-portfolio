62fef3c492a00aa7cdbb33e5b794e33d7ae0d842
vbnm134678 <syj1351@naver.com>
2024-07-25 20:15:41 +0900

    Error#2-3 route 수정

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,35 +1,62 @@
 import { Client } from '@notionhq/client';
+import { NextResponse } from 'next/server';
 import React from 'react';
 
 const getPost = React.cache(async (): Promise<any> => {
   const notionDatabase = new Client({
     auth: process.env.NOTION_API_KEY
   })
+  try {
+    const db_id = process.env.NOTION_DATABASE_ID as string;
 
-  const db_id = process.env.NOTION_DATABASE_ID;
-
-  if (db_id === undefined) {
-    const errorMessage = {
-      message: 'Check Database ID'
-    }
-    return errorMessage;
+    const posts = await new Promise(async (resolve, reject) => {
+      const postApi = await notionDatabase.databases.query({
+        database_id: db_id,
+        sorts: [
+          {
+            property: 'Name',
+            direction: 'ascending',
+          },
+        ],
+      });
+      if (postApi.status === 400) {
+        const errorMessage = 'Check Database ID'
+        reject(errorMessage);
+      } else {
+        resolve(postApi);
+      }
+    })
+    return posts;
   }
-
-  const posts = await notionDatabase.databases.query({
-    database_id: db_id,
-    sorts: [
-      {
-        property: 'Name',
-        direction: 'ascending',
-      },
-    ],
-  });
-
-  return posts;
+  catch (errorMessage) {
+    return NextResponse.json({ errorMessage }, { status: 400 });
+  }
 })
 
+// const db_id = process.env.NOTION_DATABASE_ID;
+// if (db_id === undefined) {
+//   const errorMessage: { message: string } = {
+//     message: 'Check Database ID'
+//   };
+
+// }
+// const posts = await notionDatabase.databases.query({
+//   database_id: db_id,
+//   sorts: [
+//     {
+//       property: 'Name',
+//       direction: 'ascending',
+//     },
+//   ],
+// });
+
+// const data = {
+//   message: 'notion data 가져오기',
+//   data: posts
+// }
+// let response = NextResponse.next();
+// return response.cookies.getAll(posts)
+//   // return posts;
+// })
+
 export { getPost as GET };
-
-// from: return { message: 'Failed to create' };
-
-// To: return NextResponse.json({ error: 'Failed to create' }, { status: 400 });
